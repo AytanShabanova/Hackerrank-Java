3a74510c0dffac702d1305f4b0f1460176a4868b
AytanShabanova <you@example.com>
2024-05-01 00:33:07 -0700

    first commit

---
+++ src/main/java/org/example/bitwiseOperations/Example.java
@@ -0,0 +1,12 @@
+package org.example.bitwiseOperations;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(51 & 25);//17
+        System.out.println(51 | 25); //59
+        System.out.println(51 ^ 25); //42
+        System.out.println(~23);//-24
+        System.out.println(12 << 2);
+        System.out.println(12>> 2);
+    }
+}
